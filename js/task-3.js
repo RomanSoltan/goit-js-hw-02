67798f9d9b4d63dda00983e27254ec703940705b
Roman Soltan <romabandit88@gmail.com>
2024-09-29 22:17:29 +0300

    add task_3

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,34 @@
-// console.log("hello");
+function checkForSpam(message) {
+  const wordSpam = "spam";
+  const wordSale = "sale";
+  const messageNormalize = message.toLowerCase();
+
+  if (
+    messageNormalize.includes(wordSpam) ||
+    messageNormalize.includes(wordSale)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// function checkForSpam(message) {
+//   const wordSpam = "spam";
+//   const wordSale = "sale";
+//   const messageNormalize = message.toLowerCase();
+
+//   const messageCheck =
+//     messageNormalize.includes(wordSpam) || messageNormalize.includes(wordSale)
+//       ? true
+//       : false;
+//   return messageCheck;
+// }
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
